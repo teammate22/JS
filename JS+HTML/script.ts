e49885b0c9179d5da27dd619cb47a7131a1535b3
teammate22 <timvarsey@gmail.com>
2023-04-18 20:34:51 +0300

    init

---
+++ JS+HTML/script.ts
@@ -0,0 +1,64 @@
+// задание 1
+function stringFrom(){
+  let str =''
+    for(let el of arguments){
+      str += el + ' '
+    }
+    return str
+}
+
+// задание 2
+function min(){
+  let minimum = arguments[0]
+  for(let i = 0; i < arguments.length; i++){
+    if(arguments[i+1] < minimum) {minimum = arguments[i+1]}
+  }
+  return minimum
+}
+
+// задание 3
+function numbers(){
+  let count = 0
+  for(let el of arguments){
+    if(typeof(el) == "number") count++
+  }
+  return count
+}
+// задание 4
+function mean(){
+  let leng = 0
+  let sum = 0
+  for(let el of arguments){
+    if(typeof(el) == "number"){
+      sum += el
+      leng++
+    }
+  }
+  return sum/leng
+}
+
+// задание 5
+function pow(x){
+  if(x % 2 == 0) {
+    x = x / 2
+    return pow(x)
+  }
+  if(x == 1) {return 'yes'}
+  else return 'no'
+}
+
+// задание 6
+function reverse(x: number){
+  let reversed = ''
+  while(x>9){
+   reversed += x%10
+   x = Math.floor(x/10)
+  }
+  reversed += x
+  return reversed
+}
+
+let input1 = document.getElementById('input1')
+const btn1 = document.getElementById('btn1')
+let span1 = document.getElementById('span1')
+btn1?.addEventListener('click', () => {span1.innerText = pow(input1)})
--- lesson1/script.ts
+++ lesson1/script.ts
@@ -1,5 +1,7 @@
 console.log('hello world')
 let a = 10
+let c = 5
 function b() {
   return true
 }
+prompt('print', 'print here')
