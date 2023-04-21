4ca971bfd9232d44b03b483d223e1293ed485bdc
teammate <timvarsey@gmail.com>
2023-04-21 10:48:02 +0300

    init

--- lesson1/dist/script.js
+++
@@ -1,7 +0,0 @@
-console.log('hello world');
-var a = 10;
-var c = 5;
-function b() {
-    return true;
-}
-prompt('print', 'print here');
--- lesson1/script.ts
+++
@@ -1,7 +0,0 @@
-console.log('hello world')
-let a = 10
-let c = 5
-function b() {
-  return true
-}
-prompt('print', 'print here')
